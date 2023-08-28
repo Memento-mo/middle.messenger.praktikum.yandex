7fdf223d41c20d1268531bb3e847edd6d6be4c4e
Gleb Pologov <gpologov@gmail.com>
2023-08-28 13:30:56 +0300

    fixed test

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = 3000
 app.use(cors())
 app.use(express.static('dist'))
 
+app.get('/', (req, res) => {
+  res.status().send(200)
+})
+
 app.listen(PORT, function() {
   console.log(`Example app listening on port ${PORT}!`);
 })
